d685bd5e4569b8bd1e44b1b04caa82a4a10c8ccf
erwin <erwin_komen@sil.org>
2023-03-07 08:25:59 +0100

    finetuning

--- flexche/flexche.py
+++ flexche/flexche.py
@@ -159,6 +159,7 @@ def interlinear2phonemic(oArgs):
     flInput = ""
     flOutput = ""
     method = "test1"
+    rBreak =re.compile( r'[\-\:\.]')
     namespaces = {'m': 'http://schemas.openxmlformats.org/officeDocument/2006/math',
                   'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
     
@@ -185,9 +186,11 @@ def interlinear2phonemic(oArgs):
         phoneme_lines = xmldoc.xpath("//m:t[parent::m:r[child::w:rPr/child::w:rStyle[contains(@w:val, 'ce-Latn')]]]", namespaces=namespaces)
         for el in phoneme_lines:
             # Get the text of this line
-            line = el.text
+            line = el.text.strip()
             # Convert
-            el.text = convert_word(line)
+            # el.text = convert_word(line)
+            el.text = do_convert(line)
+
 
         # Find all [Interlin Word Gloss en] lines
         gloss_lines = xmldoc.xpath("//m:e[descendant::w:rStyle[contains(@w:val, 'Word Gloss')]]", namespaces=namespaces)
@@ -196,8 +199,14 @@ def interlinear2phonemic(oArgs):
             text_lst = gloss.xpath("./descendant::m:t", namespaces=namespaces)
             gloss_txt = text_lst[0].text
             Status("Gloss = [{}]".format(gloss_txt))
+
             # Split on period
-            gloss_parts = gloss_txt.split(".")
+            # OLD gloss_parts = gloss_txt.split(".")
+
+            # Find a list of all splittable elements:'-','.'
+            gloss_break = rBreak.findall(gloss_txt)
+            gloss_parts = rBreak.split(gloss_txt)
+
             # Remove the current <m:r> child
             for mr in gloss.getchildren():
                 gloss.remove(mr)
@@ -208,7 +217,9 @@ def interlinear2phonemic(oArgs):
                     gloss_part = gloss_part.lower()
                     style = "Interlin Morpheme Gloss en"
                 # Add the period for anything but the first element
-                if idx > 0: gloss_part = "." + gloss_part
+                if idx > 0: 
+                    # gloss_part = "." + gloss_part
+                    gloss_part = gloss_break[idx-1] + gloss_part
                 # Create a child
                 child_mr = etree.fromstring('<m:r {}><m:rPr><m:nor /></m:rPr><w:rPr><w:rStyle w:val="{}" /></w:rPr><m:t>{}</m:t></m:r>'.format(
                     namespacestyle, style, gloss_part))
@@ -290,6 +301,133 @@ def convert_word(word):
         sMsg = get_error_message()
         DoError("convert_word")
     return sBack
+
+def do_convert(sPart, options={}):
+    """Convert the string in [sPart] according to the rules for the Caucasian handbook"""
+
+    try:
+        # Get the list of switches
+        switches = [] if not 'switches' in options else options['switches']
+        bGeminateV = ('vv' in switches)
+        bGeminateC = ('cc' in switches)
+        bIngush = ('ingush' in switches)
+        bHw = ('hw' in switches)
+        bGh = ('gh' in switches)
+        bIpa = ('ipa' in switches)
+        bOld = False
+
+        # Treat the 'w' where it is a hw occurring after: c, ch, k, p, sh, s, t
+        sPart = re.sub(r"(ch|c|k|p|sh|s|t)w", r"\g<1>ħ",sPart)
+
+        if bGeminateC:
+            # Treat 'ww'
+            sPart = sPart.replace("ww", "ʕʕ")
+            if not bGh:
+                # Convert gh > ʁ (long and short variant)
+                sPart = sPart.replace("ggh", "ʁː").replace("gh", "ʁ").replace("Gh", "ʁ")
+            # Convert ch > č (long and short variant)
+            sPart = sPart.replace("cch", "čč").replace("ch", "č").replace("Ch", "č")
+            # Convert zh > ž (long and short variant)
+            sPart = sPart.replace("zzh", "žž").replace("zh", "ž").replace("Zh", "ž")
+            # Convert sh > š (long and short variant)
+            sPart = sPart.replace("ssh", "šš").replace("sh", "š").replace("Sh", "š")
+            if not bHw:
+                # Convert hw > ħ (long and short variant)
+                sPart = sPart.replace("hhw", "ħħ").replace("hw", "ħ").replace("Hw", "ħ")
+        else:
+            # Treat 'ww'
+            sPart = sPart.replace("ww", "ʕː")
+            if not bGh:
+                # Convert gh > ʁ (long and short variant)
+                sPart = sPart.replace("ggh", "ʁː").replace("gh", "ʁ").replace("Gh", "ʁ")
+            # Convert ch > č (long and short variant)
+            sPart = sPart.replace("cch", "čː").replace("ch", "č").replace("Ch", "č")
+            # Convert zh > ž (long and short variant)
+            sPart = sPart.replace("zzh", "žː").replace("zh", "ž").replace("Zh", "ž")
+            # Convert sh > š (long and short variant)
+            sPart = sPart.replace("ssh", "šː").replace("sh", "š").replace("Sh", "š")
+            if not bHw:
+                # Convert hw > ħ (long and short variant)
+                sPart = sPart.replace("hhw", "ħː").replace("hw", "ħ").replace("Hw", "ħ")
+        # Convert g > ɡ (any variant)
+        sPart = sPart.replace("g", "ɡ")
+        # Convert x > χ (any variant)
+        sPart = sPart.replace("x", "χ")
+        # Treat the 'w' where it occurs in other places
+        sPart = re.sub(r"[Ww]", r"ʕ",sPart)
+        # Treat double glottal stop
+        if bGeminateC:
+            sPart = sPart.replace("''", "ʔʔ")
+            sPart = sPart.replace("’’", "ʔʔ")
+        else:
+            sPart = sPart.replace("''", "ʔː")
+            sPart = sPart.replace("’’", "ʔː")
+        # Treat single glottal stop
+        sPart = re.sub(r"([aeiuoy])(['’])", r"\g<1>ʔ", sPart)
+        # Treat [rh]
+        sPart = sPart.replace("rh", "r̥")
+
+        # The [v] does NOT change!!!
+    
+        # Make sure that ejectives have the correct apostrophe
+        sPart = sPart.replace("'", "’")
+        if bGeminateV:
+            if not bIngush:
+                sPart = sPart.replace("Yy", "üː").replace("yy", "üː")
+        else:
+            # Long vowels
+            sPart = sPart.replace("Aa", "aː").replace("aa", "aː")
+            sPart = sPart.replace("Ee", "eː").replace("ee", "eː")
+            sPart = sPart.replace("Ii", "iː").replace("ii", "iː")
+            sPart = sPart.replace("Oo", "oː").replace("oo", "oː")
+            sPart = sPart.replace("Uu", "uː").replace("uu", "uː")
+            if not bIngush:
+                sPart = sPart.replace("Yy", "üː").replace("yy", "üː")
+        # Diphthong
+        if bOld:
+            sPart = sPart.replace("Ye", "üe").replace("ye", "üe")
+            sPart = sPart.replace("Oe", "üe").replace("oe", "üe")   # So /ye/ and /oe/ coincide
+            sPart = sPart.replace("Ov", "ou").replace("ov", "ou")
+            sPart = sPart.replace("Ev", "eü").replace("ev", "eü")
+            sPart = sPart.replace("Av", "au").replace("av", "au")
+        else:
+            sPart = re.sub(r"Ye(?:^[aeiouy])", r"üe", sPart)
+            sPart = re.sub(r"Oe(?:^[aeiouy])", r"üe", sPart)
+            sPart = re.sub(r"Ov(?:^[aeiouy])", r"ou", sPart)
+            sPart = re.sub(r"Ev(?:^[aeiouy])", r"eü", sPart)
+            sPart = re.sub(r"Av(?:^[aeiouy])", r"au", sPart)
+            sPart = re.sub(r"ye(?:^[aeiouy])", r"üe", sPart)
+            sPart = re.sub(r"oe(?:^[aeiouy])", r"üe", sPart)
+            sPart = re.sub(r"ov(?:^[aeiouy])", r"ou", sPart)
+            sPart = re.sub(r"ev(?:^[aeiouy])", r"eü", sPart)
+            sPart = re.sub(r"av(?:^[aeiouy])", r"au", sPart)
+            pass
+        # SHort vowels
+        if not bIngush:
+            sPart = sPart.replace("Y", "ü").replace("y", "ü")
+        # Long consonants
+        if not bGeminateC:
+            sPart = sPart.replace("bb", "bː").replace("dd", "dː").replace("gg", "gː")
+            sPart = sPart.replace("hh", "hː").replace("kk", "kː").replace("ll", "lː")
+            sPart = sPart.replace("mm", "mː").replace("nn", "nː").replace("pp", "pː")
+            sPart = sPart.replace("qq", "qː").replace("rr", "rː").replace("ss", "sː")
+            sPart = sPart.replace("tt", "tː").replace("vv", "vː").replace("xx", "xː")
+            sPart = sPart.replace("zz", "zː")
+
+        # Should we do IPA?
+        if bIpa:
+            sPart = sPart.replace("ü", "y")
+            sPart = sPart.replace("š", "ʃ")
+            sPart = sPart.replace("ž", "ʒ")
+            sPart = sPart.replace("č", "ʧ")
+            sPart = sPart.replace("c", "ʦ")
+    except:
+        sMsg = get_error_message()
+        DoError("do_convert")
+
+    # Return what we have made of it
+    return sPart
+ 
 
 
 
